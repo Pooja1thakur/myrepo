13d0ac9a3cf153f64be3b587b36ef1542f612e2d
Pooja1thakur <pooja2810rohit@gmail.com>
2024-11-18 00:45:50 +0530

    second commit

--- index.js
+++ index.js
@@ -8,8 +8,11 @@ for (let i = 0; i < numbers.length; i++) {
 
 //Odd number array
 let number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// Loop through the array
 for (let i = 0; i < numbers.length; i++) {
+    // Check if the number is odd
   if (number[i] % 2 !== 0) { 
     console.log(number[i])
   }
 }
+
