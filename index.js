046f3491d92d39a027f8717741f0076830f40b0f
Pooja1thakur <pooja2810rohit@gmail.com>
2024-11-18 00:40:34 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+//Even number array
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+for (let i = 0; i < numbers.length; i++) {
+  if (numbers[i] % 2 === 0) { 
+    console.log(numbers[i])
+  }
+}
+
+//Odd number array
+let number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+for (let i = 0; i < numbers.length; i++) {
+  if (number[i] % 2 !== 0) { 
+    console.log(number[i])
+  }
+}
