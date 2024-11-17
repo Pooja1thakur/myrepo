a186aadbcda8643578fd2b00c1afda3c05cfb147
Pooja1thakur <pooja2810rohit@gmail.com>
2024-11-18 00:48:08 +0530

    third commit

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 //Even number array
 let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// Loop through the array
 for (let i = 0; i < numbers.length; i++) {
+     // Check if the number is odd
   if (numbers[i] % 2 === 0) { 
     console.log(numbers[i])
   }
